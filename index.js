10ce1bbd30b094af9e9a0aa4f1fcfb7548bd564d
Soungjae <soungj94@gmail.com>
2023-03-27 01:57:26 -0400

    complete lab

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
 
-const totalBatteries = batteryBatches.reduce(function (acc, el) {
-  return (acc += el);
-}, 0);
+// const totalBatteries = batteryBatches.reduce(function (acc, el) {
+//   return (acc += el);
+// }, 0);
+
+const totalBatteries = batteryBatches.reduce((acc, el) => (acc += el), 0);
